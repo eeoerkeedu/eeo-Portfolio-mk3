ef6a97a2c4a811f4c97805080ae9b39f11737ab7
eeoerke <eeoerke@gmail.com>
2022-05-09 16:34:50 -0600

    adjusted style on portfolio page, trying to get captions to not block the images

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaNpm, FaBootstrap } from "react-icons/fa";
+import { SiJavascript, SiApollographql, SiMongodb, SiMysql, SiHeroku, SiJsonwebtokens } from "react-icons/si";
+
 import "./Home.css";
 
 const Home = () => {
 	const introdata = {
 		title: "I'm Erik Oerke",
-		description: "Junior Web Dev, loving coding and working on cool projects",
+		description: "Junior Web Developer, loving coding and working on cool projects",
 	};
+	
+	const techIcons = [<SiJavascript className="p-1"/>, <FaReact className="p-1"/>, <FaHtml5 className="p-1"/>, <FaCss3Alt className="p-1"/>, <FaNodeJs className="p-1"/>, <SiApollographql className="p-1"/>, <SiMongodb className="p-1"/>,  <SiMysql className="p-1"/>,  <SiHeroku className="p-1"/>, <FaNpm className="p-1"/>, <SiJsonwebtokens className="p-1"/>, <FaBootstrap className="p-1"/>]
+
 
 	return (
 		<main>
@@ -16,17 +24,17 @@ const Home = () => {
 							<div className="align-self-center ">
 								<div className="intro mx-auto">
 									<h2 className="mb-1x">{introdata.title}</h2>
-									<h1 className="fluidz-48 mb-1x">Full Stack Web Dev</h1>
+									<h1 className="fluidz-48 mb-1x">Full Stack Web Developer</h1>
 									<p className="mb-1x">{introdata.description}</p>
 									<div className="intro_btn-action">
-										<Link to="/portfolio" >
-											<div id="button_p" className="ac_btn btn homebtn">
-												My Portfolio
-											</div>
-										</Link>
 										<Link to="/contact">
 											<div id="button_c" className="ac_btn btn homebtn">
 												Contact Me
+											</div>
+										</Link>										
+										<Link to="/portfolio" >
+											<div id="button_p" className="ac_btn btn homebtn">
+												My Portfolio
 											</div>
 										</Link>
 										<Link to="/aboutme">
@@ -35,6 +43,12 @@ const Home = () => {
 											</div>
 										</Link>
 									</div>
+									<Container fluid className="tech__icons d-block justify-content-center">
+										<h3 className="col-12">Fluent Technologies</h3>
+										{techIcons.map(icon => (
+										icon
+										))}
+									</Container>
 								</div>
 							</div>
 						</div>
--- client/src/pages/Portfolio/Portfolio.js
+++ client/src/pages/Portfolio/Portfolio.js
@@ -10,12 +10,13 @@ import { FaGithub } from "react-icons/fa";
 
 const Portfolio = () => {
     return(
-        <div className="containter d-flex carousel__holder">
+        <div className="containter d-block-flex carousel__holder">
+            <h2 className="">Portfolio</h2>
             <Carousel interval={null}>
                 <Carousel.Item>
                     <a 
                     href="https://classifyreact.herokuapp.com/"
-                    target="_blank">
+                    target="_blank" rel="noreferrer">
                         <Image
                         className="d-block project__image"
                         src={classifyImage}
@@ -25,13 +26,13 @@ const Portfolio = () => {
                     <Carousel.Caption className="carousel__captions">
                     <h3>Classify Web App</h3>
                     <p>A school or organization personnel and budget tracking application. Uses Apollo, Graph QL, React, and JWT authentication</p>
-                    <Button className="repoBtn" href="https://github.com/dkcook90/Classify_App" target="_blank"><FaGithub/> View the Repo</Button>
+                    <Button className="repoBtn" href="https://github.com/dkcook90/Classify_App" target="_blank" rel="noreferrer"><FaGithub/> View the Repo</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <a 
                     href="https://find-your-next-pet.herokuapp.com/"
-                    target="_blank">
+                    target="_blank" rel="noreferrer">
                         <Image
                         className="d-block project__image"
                         src={fynpImage}
@@ -41,13 +42,13 @@ const Portfolio = () => {
                     <Carousel.Caption className="carousel__captions">
                     <h3>Find Your Next Pet</h3>
                     <p>A site designed to help users look for pets they'd like to adopt. Uses SQL, Handlebars, and Purina API </p>
-                    <Button className="repoBtn" href="https://github.com/Ccatalyst/Find-Your-Next-Pet" target="_blank"><FaGithub/> View the Repo</Button>
+                    <Button className="repoBtn" href="https://github.com/Ccatalyst/Find-Your-Next-Pet" target="_blank" rel="noreferrer"><FaGithub/> View the Repo</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <a 
                     href="https://eeoerkeedu.github.io/Pokemon_TCG_Finder/"
-                    target="_blank">
+                    target="_blank" rel="noreferrer">
                         <Image
                         className="d-block project__image"
                         src={pokeGif}
@@ -57,13 +58,13 @@ const Portfolio = () => {
                     <Carousel.Caption className="carousel__captions">
                     <h3>Pokemon Trading Card Game Finder</h3>
                     <p>A trading card application for Pokemon. Uses two Pokemon API's and heavy CSS styling.</p>
-                    <Button className="repoBtn" href="https://github.com/eeoerkeedu/Pokemon_TCG_Finder" target="_blank"><FaGithub/> View the Repo</Button>
+                    <Button className="repoBtn" href="https://github.com/eeoerkeedu/Pokemon_TCG_Finder" target="_blank" rel="noreferrer"><FaGithub/> View the Repo</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <a 
                     href="https://eeoerkeedu.github.io/Live-Weather-Dashboard/"
-                    target="_blank">
+                    target="_blank" rel="noreferrer">
                         <Image
                         className="d-block project__image"
                         src={weatherImage}
@@ -73,13 +74,13 @@ const Portfolio = () => {
                     <Carousel.Caption className="carousel__captions">
                     <h3>Live Weather Dashboard</h3>
                     <p>A great site to get weather results for searched cities. Uses the openweather API</p>
-                    <Button className="repoBtn" href="https://github.com/eeoerkeedu/Live-Weather-Dashboard" target="_blank"><FaGithub/> View the Repo</Button>
+                    <Button className="repoBtn" href="https://github.com/eeoerkeedu/Live-Weather-Dashboard" target="_blank" rel="noreferrer"><FaGithub/> View the Repo</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <a 
                     href="https://www.teamrexgames.com/"
-                    target="_blank">
+                    target="_blank" rel="noreferrer">
                         <Image
                         className="d-block project__image"
                         src={vaultImage}
