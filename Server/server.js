d5bd8b3be25c8d8c9dbc37bef1d004246311a3ac
LilTzar <eeoerkeedu@gmail.com>
2022-05-11 11:48:13 -0600

    added server boilerplate for upload and future backend

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
